7f4be337ccca8c7d0315cb8d77f0831c6a06efd2
batboy118 <118jjh@naver.com>
2020-08-08 16:28:26 +0900

    detail 메서드 데코레이터에서 POST 삭제

--- board/views.py
+++ board/views.py
@@ -12,7 +12,7 @@ from board.serializers import PostSerializer, CommentSerializer
 def index(request, pageInfo_id):
     """
         GET     | category에 따라서 board 리스트를 보여줍니다.
-        POST    | 게시글을 등록합니다. 
+        POST    | 게시글을 등록합니다.
     """
     if request.method == "GET":
         posts = get_list_or_404(Post, pageInfo=pageInfo_id)
@@ -26,7 +26,7 @@ def index(request, pageInfo_id):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'POST', 'DELETE'])
+@api_view(['GET', 'DELETE'])
 def detail(request, post_id):
     """
         GET     | post_id 에 해당하는 post 리스트를 보여줍니다.
@@ -45,7 +45,7 @@ def detail(request, post_id):
 @api_view(['GET', 'POST'])
 def comment_list(request, post_id):
     """
-        GET     | post_id를 기준으로 comment list를 가져옵니다. 
+        GET     | post_id를 기준으로 comment list를 가져옵니다.
         POST    | post_id를 기준으로 comment를 등록합니다.
     """
     post = get_object_or_404(Post, pk=post_id)
@@ -68,6 +68,6 @@ def comment_delete(request, comment_id):
         delete | comment id를 기준으로 comment를 삭제합니다.
     """
     if request.method == "DELETE": # 댓글삭제
-        comment = get_object_or_404(Comment, pk=comment_id)        
+        comment = get_object_or_404(Comment, pk=comment_id)
         comment.delete()
         return Response({'message' : '삭제 성공!'}, status = status.HTTP_204_NO_CONTENT)
